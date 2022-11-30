be6dcd49319918490071bea3d714c2a52dd11dd6
nafets2002stefan <91226481+nafets2002stefan@users.noreply.github.com>
2022-11-30 02:34:45 +0200

    Adding basic Spring file
    Creating CRUD app

---
+++ Lab3/Application.java
@@ -0,0 +1,13 @@
+package course.springRestaurant.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
